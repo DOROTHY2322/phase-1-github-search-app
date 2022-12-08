7b3608fa566818f32f324532fd520f1c8a11adb0
DOROTHY2322 <kimelidorothy@gmail.com>
2022-12-08 23:06:42 +0300

    complete

--- js/index.js
+++ js/index.js
@@ -5,12 +5,17 @@ if(form){
 
     var search = document.getElementById("search").value
 
-
+document.getElementsByClassName("result").innerHTML=""
 
 fetch("https://api.github.com/search/users?q=octocat")
 .then((result)=> result.json())
 .then((data) =>{
   console.log(data);
+
+  document.getElementById("result").innerHTML =`
+  <img <a target="_blank" href="https://www.github.com" src="${data.avatar_url}"/></a>
+  
+  `
 })
 
   })
